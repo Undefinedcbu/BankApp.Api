83029985c6ddfc9140c0d3a542eca68d9462ff4b
irem <i_remunver@hotmail.com>
2019-10-22 15:47:17 +0300

    Transfer Api'ye eklendi.

---
+++ BankApp.Api/Controllers/TransferController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Business.Concretes;
+
+namespace BankApp.Api.Controllers
+{
+    public class TransferController : ApiController
+    {
+        HesapBusiness hesapBusiness = new HesapBusiness();
+        // GET: api/Transfer
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Transfer/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Transfer
+        public bool Post(string GonderenNo, string AliciNo, decimal Miktar)
+        {
+            return hesapBusiness.Transfer(GonderenNo, AliciNo, Miktar);
+        }
+
+        // PUT: api/Transfer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Transfer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
